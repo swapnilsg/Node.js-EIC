c223adddaf2f20e822bacaf36756a943629a1d2e
swapnil <swapnil@rocketium.com>
2020-09-13 01:50:15 +0530

    feat(express-app): add routes and tests
    add route /dependencies which displays dependencies used in this project.
    add route /minimum-secure which gives highest secure version
    add route /latest-releases which gives latest release version
    add tests to each route in test/index.js file

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
-const express = require('express')
-
+const express = require('express');
+const bent = require('bent');
+const getJson =bent('json');
+const path = require('path');
+const hbs = require('hbs');
+const semverMajor = require('semver/functions/major');
+const pkgJson = require('./package.json');
+const semverCompare = require('semver/functions/compare');
 const app = express()
 const PORT = 3000
 
-app.listen(PORT)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.get('/dependencies', (req, res) => {
+    const dependencies = pkgJson.dependencies;
+    const formatDependencies = [];
+    for ( key in dependencies) {
+        formatDependencies.push(`${key} - ${dependencies[key]}`);
+    }
+    res.render('dependencies', {layouts: 'template', dependencies: formatDependencies});
+});
+
+app.get('/minimum-secure', async (req, res) => {
+   const json = await getJson('https://nodejs.org/dist/index.json');
+   const secureVersion = {};
+   json.forEach( package => {
+       if (package.security === true ) {
+          const majorVerson = semverMajor(package.version);
+          secureVersion[majorVerson] = package;
+       }
+   });
+   const sortedKeys = Object.keys(secureVersion).sort((a,b) => a - b);
+   const sortedSecureVersions = {};
+       sortedKeys.forEach( key => {
+        sortedSecureVersions[`v${key}`] =  secureVersion[key];
+    });
+    res.json(sortedSecureVersions);
+});
+
+app.get('/latest-releases', async (req, res) => {
+    const json = await getJson('https://nodejs.org/dist/index.json');
+    const latestReleases = {};
+    json.forEach( release => {
+        const majorVerson = semverMajor(release.version);
+        if (latestReleases[`v${majorVerson}`] !== undefined) {
+            const compare = semverCompare(release.version, latestReleases[`v${majorVerson}`].version);
+            if (compare > 1)
+                latestReleases[`v${majorVerson}`] = release;
+        } else {
+            latestReleases['v'+majorVerson] =  release;
+        }
+    });
+    res.json(latestReleases);
+});
+
+app.listen(PORT);
+module.exports = app;
--- test/index.js
+++ test/index.js
@@ -1,13 +1,19 @@
 const tape = require('tape')
 const bent = require('bent')
 const getPort = require('get-port')
+const nock = require('nock');
 
 const server = require('../')
 
-const getJSON = bent('json')
-const getBuffer = bent('buffer')
+const getJSON = bent('json');
+const getBuffer = bent('buffer');
 
+const mockRes = require('./mockresponse.json');
 // Use `nock` to prevent live calls to remote services
+const scope = nock('https://nodejs.org')
+	.persist()
+	.get('/dist/index.json')
+	.reply(200, mockRes);
 
 const context = {}
 
@@ -20,14 +26,27 @@ tape('setup', async function (t) {
 })
 
 tape('should get dependencies', async function (t) {
-	const html = (await getBuffer(`${context.origin}/dependencies`)).toString()
+	const html = (await getBuffer(`${context.origin}/dependencies`)).toString();
+	t.match(html, /bent/g, 'should contain bent');
+	t.match(html, /express/g, 'should contain express');
+	t.match(html, /hbs/g, 'should contain hbs');
+	t.end();
+});
+tape('should get minimum secure versions', async t => {
+	const json = await getJSON(`${context.origin}/minimum-secure`);
+	t.equal(json['v0'].version, "v0.10.46", "v0 version should match");
+	t.equal(json['v4'].version, "v4.6.0", "v4 version should match");
+	t.end();
+});
 
-	// assertions etc
-})
-
-// more tests
+tape('should get latest-releases', async t => {
+	const json = await getJSON(`${context.origin}/latest-releases`);
+	t.equal(json['v14'].version, "v14.10.1", "v14 version should match");
+	t.equal(json['v13'].version, "v13.14.0", "v13 version should match");
+	t.end();
+});
 
 tape('teardown', function (t) {
-	context.server.close()
+	context.server.close();
 	t.end()
 })
